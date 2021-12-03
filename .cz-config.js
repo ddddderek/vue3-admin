3f60a6bceaf04bdc10e6bba69677d5368cd9a269
derek <wangyi1884@sunmooc.cn>
2021-12-03 17:19:39 +0800

    1

--- .cz-config.js
+++ .cz-config.js
@@ -16,11 +16,14 @@ module.exports = {
   // 步骤
   messages: {
     type: '请选择提交的类型',
-    customScope: '请输入修改的发范围（可选）',
+    customScope: '请输入修改的范围（可选）',
     subject: '请简要描述提交（必填）',
     body: '请输入详细描述（可选）',
     footer: '请输入要关闭的issue（可选）',
     confirm: '确认要谁用以上信息提交？（y/n）'
   },
+  // 跳过的步骤
+  skipQuestions: ['body', 'footer'],
+  // 简要描述信息字符长度
   subjectLimit: 72
 }
